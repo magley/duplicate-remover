6277487378eb986e47e7d2c95e89795ea9a2a372
magley <60552482+magley@users.noreply.github.com>
2025-12-07 11:13:25 +0100

    feat: Implement export CLI
    Advanced settings for each export type not implemented yet. I don't know how to do this from the UI standpoint.

--- source/cli/package.d
+++ source/cli/package.d
@@ -5,21 +5,28 @@ import std.array;
 import std.conv;
 import std.string;
 import std.datetime.stopwatch;
+import std.traits;
 
 import finder;
 import hasher;
 import util;
+import exporting;
 
 import vendor.clyd;
+import exporting;
 
 const int MIN_THREADS = 1;
 const int MAX_THREADS = 8;
+
+private string[] modes = [EnumMembers!FileType];
 
 void main_cli(string[] args)
 {
     Command root = new Command("duplicate-remover", "Find and remove duplicate files")
         .arg(Arg.single("dir", "d", "Directory to scan", null))
         .arg(Arg.single("workers", "w", "Number of workers", "4"))
+        .arg(Arg.single("export-type", null, "Type of export", "JSON", modes))
+        .arg(Arg.single("export-file", null, "Export desination", ""))
         .set_callback((Command cmd) { cb_scan(cmd); });
 
     handle(root, args, "duplicate-remover");
@@ -36,16 +43,33 @@ void cb_scan(Command cmd)
     long collision_time_ms = 0;
     GroupsHasher worker;
 
+    FileType export_type;
+    string export_file;
+
     // -------------------------------------------------
+    // COMPUTE ARGUMENTS
 
     dir = cmd.args["dir"].value();
     worker_count = cmd.args["workers"].integer(MIN_THREADS, MAX_THREADS);
-
-    writefln("Begin scanning %s with %d workers...", dir, worker_count);
+    export_type = cmd.args["export-type"].value().stringValToEnum!FileType;
+    export_file = cmd.args["export-file"].value_or(null);
+    if (export_file !is null && strip(export_file).empty())
+        export_file = null;
 
     version (Windows)
     {
         dir = dir.replace("/", "\\");
+        if (export_file !is null)
+            export_file = export_file.replace("/", "\\");
+    }
+
+    // -------------------------------------------------
+    // SCAN
+
+    writefln("Begin scanning %s with %d workers...", dir, worker_count);
+    if (export_file != null)
+    {
+        writefln("Export results as %s to %s", export_type, export_file);
     }
 
     sw.start();
@@ -76,4 +100,15 @@ void cb_scan(Command cmd)
     writeln("Total time: ", time_to_string(scan_time_ms + collision_time_ms));
     writeln("Found ", collisions.length, " groups");
     writeln("Found ", conflicing_files, " conflicting files");
+
+    // -------------------------------------------------
+    // EXPORT
+
+    if (export_file !is null && export_file)
+    {
+        writefln("Exporting results as %s to %s", export_type, export_file);
+
+        ExportSettings settings;
+        export_results(export_file, export_type, collisions, settings);
+    }
 }
